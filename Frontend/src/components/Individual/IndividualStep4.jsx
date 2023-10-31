4da2271063b9039b54ff70c7b2b6235840926ed5
Adeleke Moyinoluwa Solomon <pelumiiadetoye@gmail.com>
2023-10-31 14:28:21 +0100

    create account endpoint integration

--- Frontend/src/components/Individual/IndividualStep4.jsx
+++ Frontend/src/components/Individual/IndividualStep4.jsx
@@ -7,6 +7,7 @@ import { useMyContext } from "../../context";
 import Input from "../Input";
 import { IndividualStep5 } from "./IndividualStep5";
 import steps from "../../utils/data/steps.json";
+import { CreateAccountApi } from "../../utils/ApiCalls";
 
 export const IndividualStep4 = ({ setPayload, payload }) => {
   const {
@@ -21,6 +22,15 @@ export const IndividualStep4 = ({ setPayload, payload }) => {
   } = useMyContext();
 
   const [passwordError, setPasswordError] = useState(false);
+  const isStrongPassword = (password) => {
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const isPasswordMatching = (password, confirmedPassword) => {
+    return password === confirmedPassword;
+  };
 
   const [formData, setFormData] = useState({
     ...payload,
@@ -43,18 +53,26 @@ export const IndividualStep4 = ({ setPayload, payload }) => {
   };
 
   const isEmpty = formData.password === "" || formData.confirm_password === "";
-  const isPasswordMatching = formData.password === formData.confirm_password;
 
-  const handleNext = () => {
-    if (!isPasswordMatching) {
-      setPasswordError(true);
-      return;
-    }
+  const handlePayment = async () => {
+    // if (!isStrongPassword(formData.password)) {
+    //   setPasswordError(
+    //     "Password should be at least 8 characters long, contain 1 uppercase, 1 lowercase, and 1 special character."
+    //   );
+    // } else
 
-    setStepFive(true);
-    if (activeStep < steps.length - 1) {
-      console.log("activeStep", activeStep);
-      setActiveStep(activeStep + 1);
+    try {
+      if (!isPasswordMatching(formData.password, formData.confirm_password)) {
+        setPasswordError("Passwords do not match.");
+      } else {
+        setPasswordError(false);
+        const response = await CreateAccountApi(payload);
+        console.log("SignUpresponse", response);
+      }
+    } catch (error) {
+      console.error("Error creating an account", error);
+    } finally {
+      // setIsLoading(false);
     }
   };
 
@@ -67,123 +85,113 @@ export const IndividualStep4 = ({ setPayload, payload }) => {
 
   return (
     <>
-      {!StepFive && (
-        <div className="flex ">
-          {/* LEFT */}
-          <div className="lg:w-[50%] bg-base hidden lg:flex lg:flex-col lg:justify-between px-[60px] pt-8 h-screen">
-            <div className="">
-              <div className="w-[150px] h-[30px] bg-shades mb-12"></div>
-              <h1 className="md:text-thirtyPixels lg:text-[45px] font-bold leading-[50px] text-white">
-                Insurance works for you when you blah and protect
-              </h1>
-            </div>
-            <div className="md:flex md:justify-between md:items-center">
-              <img
-                src={nurse}
-                alt="An image of a Nurse"
-                width={"45%"}
-                className="h-[310px]"
-              />
-              <p className="mt-40 text-white text-eighteenPixels font-semibold cursor-pointer">
-                Help?
-              </p>
-            </div>
+      <div className="flex ">
+        {/* LEFT */}
+        <div className="lg:w-[50%] bg-base hidden lg:flex lg:flex-col lg:justify-between px-[60px] pt-8 h-screen">
+          <div className="">
+            <div className="w-[150px] h-[30px] bg-shades mb-12"></div>
+            <h1 className="md:text-thirtyPixels lg:text-[45px] font-bold leading-[50px] text-white">
+              Insurance works for you when you blah and protect
+            </h1>
           </div>
-
-          {/* RIGHT */}
-          <div className="lg:w-[50%] px-8 sm:px-10 lg:px-20 py-10  flex flex-col justify-between items-center">
-            <div>
-              <h2 className="text-twentyPixels md:text-thirtyPixels lg:text-thirtyPixels font-bold">
-                Choose a Secure Password
-              </h2>
-
-              <ProgressBar
-                activeStep={activeStep}
-                setActiveStep={setActiveStep}
-              />
-
-              <p className="mt-8 text-fourteenPixels md:text-sixteenPixels lg:text-eighteenPixels font-regular">
-                Enjoy amazing insurance services by creating your first account.
-                this is only going to take 5 minutes
-              </p>
-
-              <div className="mt-4 flex flex-col gap-2">
-                <label
-                  htmlFor="password"
-                  className="text-fourteenPixels font-semibold"
-                >
-                  Choose password
-                </label>
-                <Input
-                  className="w-full py-3 px-[8px] rounded-[7px] text-twelvePixels md:text-fourteenPixels lg:text-sixteenPixels outline-none"
-                  placeholder="************"
-                  name="password"
-                  id="password"
-                  value={formData.password}
-                  onChange={handlePayload}
-                  type="password"
-                />
-              </div>
-              <div className="mt-4 flex flex-col gap-2">
-                <label
-                  htmlFor="confirm_password"
-                  className="text-fourteenPixels font-semibold"
-                >
-                  Confirm password
-                </label>
-                <Input
-                  className="w-full py-3 px-[8px] rounded-[7px] text-twelvePixels md:text-fourteenPixels lg:text-sixteenPixels outline-none"
-                  placeholder="************"
-                  name="confirm_password"
-                  id="confirm_password"
-                  value={formData.confirm_password}
-                  onChange={handlePayload}
-                  type="password"
-                />
-                {passwordError ? (
-                  <p className="text-[14px] text-red-500">
-                    Passwords do not match!
-                  </p>
-                ) : (
-                  ""
-                )}
-              </div>
-            </div>
-
-            <div className="w-full mt-8">
-              <div className="flex justify-between gap-4">
-                <Button
-                  text="text-fourteenPixels md:text-sixteenPixels lg:text-eighteenPixels text-white font-semibold"
-                  w="w-full"
-                  onClick={handlePrevious}
-                >
-                  Previous
-                </Button>
-                <Button
-                  text="text-fourteenPixels md:text-sixteenPixels lg:text-eighteenPixels text-white font-semibold"
-                  w="w-full"
-                  onClick={handleNext}
-                  bg={`${isEmpty ? "bg-disabled" : "bg-base"}`}
-                  className={`${isEmpty ? "cursor-not-allowed" : ""}`}
-                  disabled={isEmpty && true}
-                >
-                  Next
-                </Button>
-              </div>
-              <Link to="/login">
-                <p className="text-twelvePixels md:text-thirteenPixels lg:text-fourteenPixels font-semibold text-center cursor-pointer mt-4">
-                  Already have an account?{" "}
-                  <span className="text-secondary">Sign in</span>
-                </p>
-              </Link>
-            </div>
+          <div className="md:flex md:justify-between md:items-center">
+            <img
+              src={nurse}
+              alt="An image of a Nurse"
+              width={"45%"}
+              className="h-[310px]"
+            />
+            <p className="mt-40 text-white text-eighteenPixels font-semibold cursor-pointer">
+              Help?
+            </p>
           </div>
         </div>
-      )}
 
-      {StepFive && (
-        <IndividualStep5 setFormData={setFormData} formData={formData} />
-      )}
+        {/* RIGHT */}
+        <div className="lg:w-[50%] px-8 sm:px-10 lg:px-20 py-10  flex flex-col justify-between items-center">
+          <div>
+            <h2 className="text-twentyPixels md:text-thirtyPixels lg:text-thirtyPixels font-bold">
+              Choose a Secure Password
+            </h2>
+
+            <ProgressBar
+              activeStep={activeStep}
+              setActiveStep={setActiveStep}
+            />
+
+            <p className="mt-8 text-fourteenPixels md:text-sixteenPixels lg:text-eighteenPixels font-regular">
+              Enjoy amazing insurance services by creating your first account.
+              this is only going to take 5 minutes
+            </p>
+
+            <div className="mt-4 flex flex-col gap-2">
+              <label
+                htmlFor="password"
+                className="text-fourteenPixels font-semibold"
+              >
+                Choose password
+              </label>
+              <Input
+                className="w-full py-3 px-[8px] rounded-[7px] text-twelvePixels md:text-fourteenPixels lg:text-sixteenPixels outline-none"
+                placeholder="************"
+                name="password"
+                id="password"
+                value={formData.password}
+                onChange={handlePayload}
+                type="password"
+              />
+            </div>
+            <div className="mt-4 flex flex-col gap-2">
+              <label
+                htmlFor="confirm_password"
+                className="text-fourteenPixels font-semibold"
+              >
+                Confirm password
+              </label>
+              <Input
+                className="w-full py-3 px-[8px] rounded-[7px] text-twelvePixels md:text-fourteenPixels lg:text-sixteenPixels outline-none"
+                placeholder="************"
+                name="confirm_password"
+                id="confirm_password"
+                value={formData.confirm_password}
+                onChange={handlePayload}
+                type="password"
+              />
+              {passwordError && (
+                <p className="text-[14px] text-red-500">{passwordError}</p>
+              )}
+            </div>
+          </div>
+
+          <div className="w-full mt-8">
+            <div className="flex justify-between gap-4">
+              <Button
+                text="text-fourteenPixels md:text-sixteenPixels lg:text-eighteenPixels text-white font-semibold"
+                w="w-full"
+                onClick={handlePrevious}
+              >
+                Previous
+              </Button>
+              <Button
+                text="text-fourteenPixels md:text-sixteenPixels lg:text-eighteenPixels text-white font-semibold"
+                w="w-full"
+                onClick={handlePayment}
+                bg={`${isEmpty ? "bg-disabled" : "bg-base"}`}
+                className={`${isEmpty ? "cursor-not-allowed" : ""}`}
+                disabled={isEmpty && true}
+              >
+                Proceed to Payment
+              </Button>
+            </div>
+            <Link to="/login">
+              <p className="text-twelvePixels md:text-thirteenPixels lg:text-fourteenPixels font-semibold text-center cursor-pointer mt-4">
+                Already have an account?{" "}
+                <span className="text-secondary">Sign in</span>
+              </p>
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- Frontend/src/components/Individual/IndividualStep5.jsx
+++ Frontend/src/components/Individual/IndividualStep5.jsx
@@ -5,6 +5,7 @@ import { Button } from "../Button/Button";
 import { Link } from "react-router-dom";
 import { useMyContext } from "../../context";
 import Input from "../Input";
+import { CreateAccountApi, SubscriptionApi } from "../../utils/ApiCalls";
 
 export const IndividualStep5 = ({ setFormData, formData }) => {
   const {
@@ -20,7 +21,6 @@ export const IndividualStep5 = ({ setFormData, formData }) => {
   const [payload, setPayload] = useState({
     ...formData,
     plan_duration: "Choose duration",
-    hospitalSize: "",
   });
 
   console.log("payload", payload);
@@ -33,7 +33,17 @@ export const IndividualStep5 = ({ setFormData, formData }) => {
     }));
   };
 
-  const handlePayment = () => {};
+  const handlePayment = async () => {
+    try {
+      // setIsLoading(true);
+      const response = await CreateAccountApi(payload);
+      console.log("SignUpresponse", response);
+    } catch (error) {
+      console.error("Error creating an account", error);
+    } finally {
+      // setIsLoading(false);
+    }
+  };
 
   const handlePrevious = () => {
     setStepFive(false);
@@ -43,6 +53,68 @@ export const IndividualStep5 = ({ setFormData, formData }) => {
   };
 
   const isPlanDurationSelected = payload.plan_duration === "Choose duration";
+
+  const handleHospitalSizeChange = (e) => {
+    const { name, value } = e.target;
+    setPayload((prevPayload) => ({
+      ...prevPayload,
+      [name]: value,
+    }));
+
+    // HospitalSize();
+  };
+
+  // const HospitalSize = async () => {
+  //   try {
+  //     const response = await SubscriptionApi({
+  //       hospitalSize: payload.hospitalSize,
+  //     });
+  //     console.error("HospitalSizeRes", response);
+  //   } catch (error) {
+  //     console.error("Error fetching hospital size:", error);
+  //   }
+  // };
+
+  const detailsByHospitalSize = {
+    "1-20": {
+      cost: "₦50,000",
+      coverage: "Up to 50,000 NGN coverage.",
+      bedsCovered: "Cover up to 20 beds.",
+      staffCovered: "Cover for 8 staff.",
+    },
+    "21-50": {
+      cost: "₦70,000",
+      coverage: "Up to 70,000 NGN coverage.",
+      bedsCovered: "Cover up to 50 beds.",
+      staffCovered: "Cover for 13 staff.",
+    },
+    "51-100": {
+      cost: "₦130,000",
+      coverage: "Up to 130,000 NGN coverage.",
+      bedsCovered: "Cover up to 100 beds.",
+      staffCovered: "Cover for 20 staff.",
+    },
+    "101-500": {
+      cost: "₦180,000",
+      coverage: "Up to 180,000 NGN coverage.",
+      bedsCovered: "Cover up to 500 beds.",
+      staffCovered: "Cover for 50 staff.",
+    },
+    "501-1000": {
+      cost: "₦250,000",
+      coverage: "Up to 250,000 NGN coverage.",
+      bedsCovered: "Cover up to 1000 beds.",
+      staffCovered: "Cover for 200 staff.",
+    },
+    // "1001 and above": {
+    //   cost: "Custom",
+    //   coverage: "Up to 250,000 NGN coverage.",
+    //   bedsCovered: "Cover up to 1000 beds.",
+    //   staffCovered: "Cover for 200 staff.",
+    // },
+  };
+
+  const selectedHospitalDetails = detailsByHospitalSize[payload.hospitalSize];
 
   return (
     <div className="flex">
@@ -90,20 +162,18 @@ export const IndividualStep5 = ({ setFormData, formData }) => {
               name="hospitalSize"
               id="hospitalSize"
               value={payload.hospitalSize}
-              onChange={handlePayload}
+              onChange={handleHospitalSizeChange}
               className="w-full py-3 rounded-[7px] border border-gray px-[8px] text-twelvePixels md:text-fourteenPixels lg:text-sixteenPixels outline-none"
             >
-              {/* 50, 70, 130, 180, 250, Custom */}
-              <option value="Choose bed number">Choose hospital size</option>
-              <option value="1-20 beds">1-20 beds</option>
-              <option value="21-50 beds">21-50 beds</option>
-              <option value="51-100 beds">51-100 beds</option>
-              <option value="101-500 beds">101-500 beds</option>
-              <option value="101-500 beds">501 and above</option>
+              <option value="1-20">1-20</option>
+              <option value="21-50">21-50</option>
+              <option value="51-100">51-100</option>
+              <option value="101-500">101-500</option>
+              <option value="501-1000">501-1000</option>
             </select>
           </div>
 
-          <div className="border border-gray mt-8 rounded-[8px] px-4 py-6">
+          {/* <div className="border border-gray mt-8 rounded-[8px] px-4 py-6">
             <h4 className="text-[16px] md:text-[20px] lg:text-[24px] text-base font-semibold">
               ₦50,000/
               <span className="text-[14px] md:text-[16px] lg:text-[18px] text-[#00000080] font-bold">
@@ -126,6 +196,30 @@ export const IndividualStep5 = ({ setFormData, formData }) => {
               </li>
               <li className="text-[10px] md:text-[12px] lg:text-[14px] text-[#00000080] font-bold">
                 - Cover for 13 staff.{" "}
+              </li>
+              <li className="text-[10px] md:text-[12px] lg:text-[14px] text-[#00000080] font-bold">
+                - Access to educational and risk management materials.{" "}
+              </li>
+            </ul>
+          </div> */}
+          <div className="border border-gray mt-8 rounded-[8px] px-4 py-6">
+            <h4 className="text-[16px] md:text-[20px] lg:text-[24px] text-base font-semibold">
+              {selectedHospitalDetails?.cost}/
+              <span className="text-[14px] md:text-[16px] lg:text-[18px] text-[#00000080] font-bold">
+                Annum
+              </span>
+            </h4>
+
+            <p className="text-[#00000080] mt-4 text-twelvePixels md:text-fourteenPixels lg:text-sixteenPixels">
+              {selectedHospitalDetails?.coverage}
+            </p>
+
+            <ul className="mt-4">
+              <li className="text-[10px] md:text-[12px] lg:text-[14px] text-[#00000080] font-bold">
+                - {selectedHospitalDetails?.bedsCovered}
+              </li>
+              <li className="text-[10px] md:text-[12px] lg:text-[14px] text-[#00000080] font-bold">
+                - {selectedHospitalDetails?.staffCovered}
               </li>
               <li className="text-[10px] md:text-[12px] lg:text-[14px] text-[#00000080] font-bold">
                 - Access to educational and risk management materials.{" "}
