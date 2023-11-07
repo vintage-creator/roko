245a5f45d196e682d76007f5d24c83a552b4cbb5
Israel Abazie <you@example.com>
2023-11-07 01:46:01 +0100

    proxy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,4 +3,33 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/auth': {
+        target: 'https://rokoui.onrender.com', 
+        changeOrigin: true
+      },
+  //     '/admin': {
+  //       target: 'http://localhost:8000', 
+  //       changeOrigin: true
+  //     },
+  //     '/subscribe': {
+  //       target: 'http://localhost:8000', 
+  //       changeOrigin: true
+  //     },
+  //     '/user': {
+  //       target: 'http://localhost:8000', 
+  //       changeOrigin: true
+  //     },
+  //     '/contact': {
+  //       target: 'http://localhost:8000', 
+  //       changeOrigin: true
+  //     },
+  //     '/wh': {
+  //       target: 'http://localhost:8000', 
+  //       changeOrigin: true
+  //     },
+      
+    }
+  }
 });
