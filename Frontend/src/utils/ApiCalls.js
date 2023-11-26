2c126fe08af49fee9689516c442f333ad99c469c
Israel Abazie <101069893+vintage-creator@users.noreply.github.com>
2023-11-26 15:40:57 +0100

    Update ApiCalls.js

--- Frontend/src/utils/ApiCalls.js
+++ Frontend/src/utils/ApiCalls.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:8000";
+const baseURL = "";
 
 export const CreateAccountApi = (Payload) => {
   console.log("CreateAccountApi", Payload);
