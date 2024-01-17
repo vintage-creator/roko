39c75dc52de528d6f7c84c3a0d24d14149d93641
Adeleke Moyinoluwa Solomon <pelumiiadetoye@gmail.com>
2024-01-17 17:25:10 +0100

    payment gateway

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-// import { SignUpSuccess } from "./components/SignUpSuccess";
 import { NavRoutes } from "./routes/NavRoutes";
 
 function App() {
   return (
     <div>
       <NavRoutes />
-      {/* <SignUpSuccess /> */}
     </div>
   );
 }
