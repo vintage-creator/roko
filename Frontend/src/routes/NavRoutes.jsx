5014ed048791580bb3f3a79f9a8dc59e43a0602a
Adeleke Moyinoluwa Solomon <pelumiiadetoye@gmail.com>
2024-02-02 13:02:54 +0100

    made changes

--- Frontend/src/routes/NavRoutes.jsx
+++ Frontend/src/routes/NavRoutes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
 import ForgotPassword from "../pages/ForgotPassword";
@@ -13,9 +13,23 @@ import { SingleCourse } from "../pages/Courses/ExploreCourses/SingleCourse";
 import Verification from "../pages/EmailVerify/Verification";
 import { useMyContext } from "../context";
 import { EmailVerified } from "../pages/EmailVerify/EmailVerified";
+import ReactDOM from "react-dom";
 
 export const NavRoutes = () => {
   const { isAuthenticated } = useMyContext();
+
+  useEffect(() => {
+    // Check the URL parameters for payment success
+    const urlParams = new URLSearchParams(window.location.search);
+    const paymentStatus = urlParams.get("status");
+
+    // Check if payment is successful and render IndividualStep4
+    if (paymentStatus === "successful") {
+      // Render the IndividualStep4 component directly
+      ReactDOM.render(<IndividualStep4 />, document.getElementById("root"));
+    }
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -28,6 +42,11 @@ export const NavRoutes = () => {
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/auth/verify" element={<Verification />} />
         <Route path="/email-verification" element={<EmailVerified />} />
+
+        <Route
+          path="/wh/confirm-payment/IndividualStep4"
+          element={<IndividualStep4 />}
+        />
 
         <Route
           path="/dashboard"
